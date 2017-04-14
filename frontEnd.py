fb6dbb8302276f7637aed49eebb34655e5f0a314
AJB117 <psoga117@gmail.com>
2017-04-13 21:48:21 -0700

    Update frontEnd.py

--- frontEnd.py
+++ frontEnd.py
@@ -1,36 +1,27 @@
 import Tkinter as Tk
 import backEnd
 
+# Main window
 window = Tk.Tk()
 window.title("Cipher Encrypt/Decrypt")
 window.geometry("900x600")
 
-lbl = Tk.Label(window, text="")
-lbl.pack()
-
-i = 0
-
-
-def increment():
-    global i
-    i += 1
-    lbl.configure(text="%d" % i)
-
-btn = Tk.Button(window, text="Count", command=increment)
-btn.pack()
-
-encrypt = Tk.Text(window, height=20, width=40)
-encrypt.pack()
+# Where users put their text to encrypt (Caesar)
+Caesar_encrypt = Tk.Text(window, height=20, width=40)
+Caesar_encrypt.pack()
 
 
+# Passing plaintext to caesar function in backend
 def backend_caesar():
-    x = encrypt.get("1.0", 'end-1c')
+    x = Caesar_encrypt.get("1.0", 'end-1c')
     print backEnd.caesar_1(x)
 
+# Submit plaintext to function to backend
 submit = Tk.Button(window, text="Submit", command=backend_caesar)
 submit.pack()
 
-decrypt = Tk.Text(window, height=20, width=40)
-decrypt.pack()
+# Where users put their text to decrypt (Caesar)
+Caesar_decrypt = Tk.Text(window, height=20, width=40)
+Caesar_decrypt.pack()
 
 window.mainloop()
