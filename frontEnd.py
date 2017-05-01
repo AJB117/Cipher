86782cdeece513833ac607ebd5bcf0782ca20ff7
AJB117 <psoga117@gmail.com>
2017-04-30 17:03:51 -0700

    Update frontEnd.py

--- frontEnd.py
+++ frontEnd.py
@@ -1,6 +1,30 @@
 import Tkinter as Tk
 import backEnd
 
+# Functions
+
+# Passing plaintext to caesar function in backend; create new window to show selectable
+# encrypted string
+def get_caesar():
+    win = Tk.Toplevel(window)
+    output_box = Tk.Text(win, height=300, width=300)
+    output_box.pack()
+    shift = var.get()
+    output_box.insert(Tk.END, backEnd.caesar_e(Caesar_encrypt.get("1.0", 'end-1c'), shift), "encrypt")
+    output_box.configure(state=Tk.DISABLED)
+
+# Passing encrypted text to caesar decryption function in backend; create new window
+# to show selectable encrypted string
+def dget_caesar():
+    win = Tk.Toplevel(window)
+    output_box = Tk.Text(win, height=300, width=300)
+    output_box.pack()
+    shift = decrypt_var.get()
+    output_box.insert(Tk.END, backEnd.caesar_d(Caesar_decrypt.get("1.0", 'end-1c'), shift), "decrypt")
+    output_box.configure(state=Tk.DISABLED)
+
+# Needs functions to pass plaintext/encrypted text to backend for vigenere
+
 # Main window
 window = Tk.Tk()
 window.title("Cipher Encrypt/Decrypt")
@@ -20,16 +44,6 @@ var.set(1)
 Caesar_key = Tk.OptionMenu(window, var, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                            14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
 Caesar_key.pack(side='left')
-
-# Passing plaintext to caesar function in backend; create new window to show selectable
-# encrypted string
-def get_caesar():
-    win = Tk.Toplevel(window)
-    output_box = Tk.Text(win, height=300, width=300)
-    output_box.pack()
-    shift = var.get()
-    output_box.insert(Tk.END, backEnd.caesar_e(Caesar_encrypt.get("1.0", 'end-1c'), shift))
-    output_box.configure(state=Tk.DISABLED)
     
 # Submit plaintext to function to backend
 Caesar_submit = Tk.Button(window, text="Caesar Submit", command=get_caesar)
@@ -50,21 +64,32 @@ Caesar_dkey = Tk.OptionMenu(window, decrypt_var, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
                             14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26)
 Caesar_dkey.pack(side='left')
 
-# Passing encrypted text to caesar decryption function in backend; create new window
-# to show selectable encrypted string
-def dget_caesar():
-    win = Tk.Toplevel(window)
-    output_box = Tk.Text(win, height=300, width=300)
-    output_box.pack()
-    shift = decrypt_var.get()
-    output_box.insert(Tk.END, backEnd.caesar_d(Caesar_decrypt.get("1.0", 'end-1c'), shift))
-
 # Submit encrypted text to function to backend
 Caesar_dsubmit = Tk.Button(window, text="Caesar Decrypt Submit", command=dget_caesar)
 Caesar_dsubmit.pack(side='left')
  
-# TODO
-# Vigenere UI in the same style as Caesar boxes
-# Beautification
+# Vigenere layout
+
+vigenere_encrypt = Tk.Text(window, height=20, width=40)
+vigenere_encrypt.grid(row=2, column=0)
+
+keyfield = Tk.Text(window, height=5, width=10)
+keyfield.grid(padx=10, row=2, column=1)
+
+vigenere_decrypt = Tk.Text(window, height=20, width=40)
+vigenere_decrypt.grid(row=2, column=3)
+
+# Button needs a command
+vigenere_submit = Tk.Button(window, text="Vigenere Submit")
+vigenere_submit.grid(row=2, column=2)
+
+dsubmit_keyfield = Tk.Text(window, height=5, width=10)
+dsubmit_keyfield.grid(padx=10, row=2, column=4)
+
+# Button needs a command
+vigenere_dsubmit = Tk.Button(window, text="Vigenere Decrypt Submit")
+vigenere_dsubmit.grid(row=2, column=5)
+
+
 
 window.mainloop()
