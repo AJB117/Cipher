ad9a6eafa3a24e7f424a3c822cc362eb29ef3d93
jabb3rw0cky <true.sarmiento@gmail.com>
2017-04-30 20:46:44 -0700

    Update backEnd.py

--- backEnd.py
+++ backEnd.py
@@ -24,4 +24,28 @@ def caesar(caesar_out, shift, e_or_d):
 
 
 # TODO
+# PS limit the key to 5 letters
+# finish the message encryption i have no clue how to make array loops
 # Vigenere functionality
+def vigenere(vigenere_out, key, e_or_d):
+    key = [1,2,3,4,5]
+    if key == "encrypt":
+        for [i] in key:
+            if not key[i].isalpha():
+                temp += chr(ord(key[i])):
+            else:
+                if key[i].islower():
+                    temp += chr((((ord(key[i]) - 97) + shift) % 26) + 97)
+            else:
+                temp += chr((((ord(key[i]) -65) + shift) % 26) + 65)
+        return temp
+    elif e_or_d == "decrypt":
+        for [i] in key:
+            if not key[i].isalpha():
+                temp += chr(ord(key[i])):
+            else:
+                if key[i].islower():
+                    temp += chr((((ord(key[i]) - 97) + shift) % 26) + 97)
+            else:
+                temp += chr((((ord(key[i]) -65) + shift) % 26) + 65)
+        return temp
